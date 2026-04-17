a6962199af6120f28b331a22dcc4e2e09fdb21e0
Simran-ubhi <simranubhi68@gmail.com>
2026-04-16 22:15:32 -0400

    new product form setup

---
+++ app/admin/products/new/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation"
+
+
+type Product = {
+    id: string;
+    name: string;
+    price: string;
+};
+
+export default function NewProductPage(){
+    const router = useRouter();
+
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState("");
+    const [product, setProduct] = useState<Product[]>([]);
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const newProduct: Product = {
+            id: Date.now().toString(),
+            name,
+            price,
+        };
+
+        setProduct((prev)=>[...prev, newProduct]);
+
+        // reset form
+
+        setName("");
+        setPrice("");
+
+        router.push("/admin/products");
+    }
+
+    return (
+        <div>
+            <h1>Add Product</h1>
+            
+            <form onSubmit={handleSubmit}>
+                <input 
+                style={{padding:10, margin:20}}
+                type="text" 
+                placeholder="Product Name" 
+                value = {name} 
+                onChange={(e)=> setName(e.target.value)} 
+                />
+
+                <input 
+                type="number"
+                style={{padding:10, margin:20}}
+                placeholder="price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                />
+
+                <button type="submit">Submit</button>
+            </form>
+
+            <h2>Preview</h2>
+            <ul>
+                {product.map((p) => (
+                    <li key={p.id}>
+                        {p.name} - ${p.price}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
